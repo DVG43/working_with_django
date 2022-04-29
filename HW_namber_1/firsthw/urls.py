2c5a297c95e482434f579e21e0c53d4350e0e241
Galuta Dmitrii <dvgaluta@gmail.com>
2022-04-29 23:36:15 +0300

    commit_5

--- HW_namber_1/firsthw/urls.py
+++ HW_namber_1/firsthw/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app.views import home_view, time_view, workdir_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', home_view),
+    path('', time_view),
+    path('', workdir_view)
+    ]
